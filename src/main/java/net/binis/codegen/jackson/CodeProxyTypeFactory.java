c8afb060ccc5b266889388377a3729b0df298478
Binis <binisbelev@gmail.com>
2023-12-10 16:00:16 +0200

    Better collection detection handling,

--- src/main/java/net/binis/codegen/jackson/CodeProxyTypeFactory.java
+++ src/main/java/net/binis/codegen/jackson/CodeProxyTypeFactory.java
@@ -24,29 +24,43 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.type.ClassStack;
 import com.fasterxml.jackson.databind.type.TypeBindings;
 import com.fasterxml.jackson.databind.type.TypeFactory;
-import com.fasterxml.jackson.databind.util.LookupCache;
+import lombok.extern.slf4j.Slf4j;
 import net.binis.codegen.factory.CodeFactory;
 import net.binis.codegen.tools.Reflection;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static java.util.Objects.nonNull;
+import static net.binis.codegen.tools.Reflection.loadClass;
+import static net.binis.codegen.tools.Tools.with;
 
+@Slf4j
 public class CodeProxyTypeFactory extends TypeFactory {
 
-    @SuppressWarnings("unchecked")
+    protected static Set<Class<?>> collections = initCollections();
+
+    protected static Set<Class<?>> initCollections() {
+        var result = new HashSet<Class<?>>();
+        result.add(Map.class);
+        result.add(List.class);
+        result.add(Set.class);
+        result.add(Collection.class);
+        with(loadClass("java.util.SequencedCollection"), result::add);
+        return result;
+    }
+
     public CodeProxyTypeFactory(TypeFactory parent) {
-        super((LookupCache) Reflection.getFieldValue(parent, "_typeCache"));
+        super(Reflection.getFieldValue(parent, "_typeCache"));
     }
 
     @Override
     protected JavaType _fromClass(ClassStack context, Class<?> rawType, TypeBindings bindings) {
-        var type = rawType.equals(Map.class) || rawType.equals(Set.class) || rawType.equals(List.class) ? null : CodeFactory.lookup(rawType);
+        var type = collections.contains(rawType) ? null : CodeFactory.lookup(rawType);
         if (nonNull(type)) {
+            log.info("{}", type.getCanonicalName());
             return super._fromClass(context, type, bindings);
         }
+        log.info("{}", rawType.getCanonicalName());
         return super._fromClass(context, rawType, bindings);
     }
 
