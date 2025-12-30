9349870c23491ce5a2e1a64354e5cc9fb50fc299
BINIS\binis <binisbelev@gmail.com>
2025-12-30 21:38:54 +0200

    Moved MappingKeys from spring-configuration to jackson

--- src/main/java/net/binis/codegen/jackson/CodeJackson.java
+++ src/main/java/net/binis/codegen/jackson/CodeJackson.java
@@ -24,19 +24,25 @@ import net.binis.codegen.annotation.CodeConfiguration;
 import net.binis.codegen.exception.MapperException;
 import net.binis.codegen.exception.ValidationFormException;
 import net.binis.codegen.factory.CodeFactory;
+import net.binis.codegen.jackson.mapping.keys.MappingKeys;
 import net.binis.codegen.jackson.serialize.CodeEnumStringSerializer;
 import net.binis.codegen.map.Mapper;
+import net.binis.codegen.tools.Reflection;
 import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.json.JsonMapper;
 import tools.jackson.databind.module.SimpleModule;
 
 import java.util.Map;
 
+import static java.util.Objects.nonNull;
+
 @CodeConfiguration
 public class CodeJackson {
 
     public static void initialize() {
         CodeFactory.registerType(ObjectMapper.class, CodeFactory.lazy(CodeJackson::getMapper));
+        CodeFactory.registerType(JsonMapper.class, CodeFactory.lazy(CodeJackson::getMapper));
+
         Mapper.registerMapper(String.class, Object.class, (source, destination) -> {
             try {
                 return CodeFactory.create(ObjectMapper.class).readerForUpdating(destination).readValue(source);
@@ -57,6 +63,42 @@ public class CodeJackson {
             }
         });
 
+        Mapper.map().key(MappingKeys.JSON).source(Object.class).destination(String.class).producer(o -> {
+            try {
+                return CodeFactory.create(ObjectMapper.class).writeValueAsString(o);
+            } catch (Exception e) {
+                return "{ \"exception\": \"" + e.getMessage() + "\"}";
+            }
+        });
+        Mapper.registerMapper(String.class, Object.class, MappingKeys.JSON, (source, destination) -> {
+            try {
+                return CodeFactory.create(ObjectMapper.class).readerForUpdating(destination).readValue(source);
+            } catch (ValidationFormException v) {
+                throw v;
+            } catch (Exception e) {
+                throw new MapperException(e);
+            }
+        });
+
+        var xml = Reflection.loadClass("tools.jackson.dataformat.xml.XmlMapper");
+        if (nonNull(xml)) {
+            Mapper.map().key(MappingKeys.XML).source(Object.class).destination(String.class).producer(o -> {
+                try {
+                    return ((ObjectMapper) CodeFactory.create(xml)).writeValueAsString(o);
+                } catch (Exception e) {
+                    return "<exception>" + e.getMessage() + "</exception>";
+                }
+            });
+            Mapper.registerMapper(String.class, Object.class, MappingKeys.XML, (source, destination) -> {
+                try {
+                    return ((ObjectMapper) CodeFactory.create(xml)).readerForUpdating(destination).readValue(source);
+                } catch (ValidationFormException v) {
+                    throw v;
+                } catch (Exception e) {
+                    throw new MapperException(e);
+                }
+            });
+        }
     }
 
     public static ObjectMapper getMapper() {
